bc884d760ab206f572a85c864ac0d4a376b7fad2
zhangzijian <1551324058@qq.com>
2022-02-14 23:39:37 +0800

    push commit

--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -9,6 +9,7 @@ public class gitTest {
         System.out.println("hello git4");
         System.out.println("hot-fix test");
         System.out.println("master test");
+        System.out.println("push test");
     }
 
 }
